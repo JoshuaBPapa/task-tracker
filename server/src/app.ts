e7052f01619a07cf42635bf46da52c0d95584afd
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-07-24 17:00:32 +0100

    added auth routes and error middleware to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import { authRouter } from './routes';
+import { errorMiddleware } from './middleware/error-middleware';
 
 dotenv.config();
 
@@ -11,6 +13,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use('/auth', authRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
