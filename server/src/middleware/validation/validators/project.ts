3420b2fd1c9d0dabf491adc5f9d0607b11cf9cb6
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-07-26 12:16:41 +0100

    removed redundant generics

--- server/src/middleware/validation/validators/project.ts
+++ server/src/middleware/validation/validators/project.ts
@@ -1,10 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
-import { CreateProjectReqBody, TokenData } from '../../../types';
 import { runValidationChecks } from '../run-validation-chains';
 import { checkProjectName } from '../chains';
 
 export const projectValidator = async (
-  req: Request<any, any, CreateProjectReqBody>,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void | NextFunction> => {
