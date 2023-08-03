ea19431fd8045fb1ef294f2e3a5ca41f16a7898e
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-08-03 16:38:58 +0100

    removed redundant import

--- server/src/types/req-params/get-tasks-params.ts
+++ server/src/types/req-params/get-tasks-params.ts
@@ -1,4 +1,4 @@
-import { TaskFilterParams, UsersFilterParams } from './filter-req-params';
+import { TaskFilterParams } from './filter-req-params';
 import { OrderByReqParams } from './order-by-req-params';
 import { PaginatedReqParams } from './paginated-req-params';
 import { SearchReqParams } from './search-req-params';
