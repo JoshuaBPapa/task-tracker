11f915a5f2d58b09430f901559c303974d6c3818
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-08-03 11:31:22 +0100

    fixed filter query string not working with multiple values

--- server/src/helpers/build-filter-query-string.ts
+++ server/src/helpers/build-filter-query-string.ts
@@ -1,9 +1,9 @@
-// convert query params like status=4,2,3 to an SQL query string like (status = 4 OR 2 OR 3)
+// convert query params like status=4,2,3 to an SQL query string like (status = 4 OR status = 2 OR status = 3)
 export const buildFilterQueryString = (
   filterValues: string | undefined,
   column: string
 ): string => {
   if (!filterValues) return '';
-  const convertedValues = filterValues.replace(/,/g, ' OR ');
+  const convertedValues = filterValues.replace(/,/g, ` OR ${column} = `);
   return `AND (${column} = ${convertedValues})`;
 };
