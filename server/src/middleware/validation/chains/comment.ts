708784d54581a9c88f9cab22825e23a78a808ea4
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-08-17 14:45:35 +0100

    added validation to POST comments to ensure comments can only be added to tasks belonging to the user's team

--- server/src/middleware/validation/chains/comment.ts
+++ server/src/middleware/validation/chains/comment.ts
@@ -1,5 +1,8 @@
 import { ValidationChain, body } from 'express-validator';
 import { intOnlyMsg, maxLengthMsg, requiredMsg } from '../error-messages';
+import { Response } from 'express';
+import { TokenData } from '../../../types';
+import { selectTaskById } from '../../../services';
 
 export const checkComment = (): ValidationChain => {
   return body('comment')
@@ -12,4 +15,11 @@ export const checkComment = (): ValidationChain => {
 
 export const checkTaskId = (): ValidationChain => {
   return body('taskId').isInt().withMessage(intOnlyMsg());
+};
+
+export const checkTaskBelongsToTeam = (res: Response<any, TokenData>): ValidationChain => {
+  return body('taskId').custom(async (taskId: number) => {
+    const findTask = await selectTaskById(res.locals.teamId, taskId);
+    if (!findTask[0].length) return Promise.reject('This task does not belong to this team');
+  });
 };
--- server/src/middleware/validation/validators/comment.ts
+++ server/src/middleware/validation/validators/comment.ts
@@ -1,13 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
-import { checkComment, checkTaskId } from '../chains';
+import { checkComment, checkTaskBelongsToTeam, checkTaskId } from '../chains';
 import { runValidationChecks } from '../run-validation-chains';
+import { TokenData } from '../../../types';
 
 export const commentValidator = async (
   req: Request,
-  res: Response,
+  res: Response<any, TokenData>,
   next: NextFunction
 ): Promise<void | NextFunction> => {
-  await runValidationChecks(req, [checkComment(), checkTaskId()], true);
+  await runValidationChecks(
+    req,
+    [checkComment(), checkTaskId(), checkTaskBelongsToTeam(res)],
+    false
+  );
 
   next();
 };
