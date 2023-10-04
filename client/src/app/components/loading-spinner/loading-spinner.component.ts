3ef52449aa8263990d8ded10228e49d84f9c61a9
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-10-04 15:58:17 +0100

    added a loading spinner component

---
+++ client/src/app/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoadingSpinnerComponent {}
