538fdaa3ebb8f01aa9a7a2e86af9cbecfbd28934
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-11-22 12:45:29 +0000

    set logged in user from access token in localStorage on app init, or set as null if no valid token present

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -3,19 +3,28 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { AuthService } from './core/services/auth/auth.service';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
+  let app: AppComponent;
+  const authServiceSpy = jasmine.createSpyObj('AuthService', ['setLoggedInUser']);
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, ToastModule],
-      providers: [MessageService],
+      providers: [MessageService, { provide: AuthService, useValue: authServiceSpy }],
       declarations: [AppComponent],
-    })
-  );
+    });
+    const fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should call authService.setLoggedInUser on init', () => {
+    app.ngOnInit();
+    expect(authServiceSpy.setLoggedInUser).toHaveBeenCalled();
   });
 });
