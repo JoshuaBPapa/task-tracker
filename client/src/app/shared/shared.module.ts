d48adcfd3005a79f2d5fa239ff4b0ced17f29318
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-11-22 13:31:30 +0000

    added RouterModule to SharedModule

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimengModule } from './primeng/primeng.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PrimengModule, ReactiveFormsModule],
-  exports: [CommonModule, PrimengModule, ReactiveFormsModule],
+  imports: [CommonModule, PrimengModule, ReactiveFormsModule, RouterModule],
+  exports: [CommonModule, PrimengModule, ReactiveFormsModule, RouterModule],
 })
 export class SharedModule {}
