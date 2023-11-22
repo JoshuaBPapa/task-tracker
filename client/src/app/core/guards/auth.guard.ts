2aa246e8d7bc567b7c955f43d15899826c6c012d
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-11-22 11:53:46 +0000

    updated authGuard to prevent logged in users accessing the auth page

--- client/src/app/core/guards/auth.guard.ts
+++ client/src/app/core/guards/auth.guard.ts
@@ -1,12 +1,26 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const { url } = state;
 
-  if (!authService.loggedInUser) {
+  if (url === '/auth' && authService.loggedInUser) {
+    router.navigateByUrl('/');
+    return false;
+  }
+
+  if (url !== '/auth' && !authService.loggedInUser) {
     router.navigateByUrl('/auth');
     return false;
   }
