fe2a1d851f9d6eff9049c42fb875823958c29bb5
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-12-13 16:57:20 +0000

    created a search bar component

---
+++ client/src/app/components/inputs/search-input/search-input.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  OnInit,
+  OnDestroy,
+  Input,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { UnsubscribeService } from 'src/app/services/unsubscribe.service';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  selector: 'app-search-input',
+  standalone: true,
+  imports: [SharedModule],
+  providers: [UnsubscribeService],
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchInputComponent implements OnInit, OnDestroy {
+  @Input() placeholder = 'Begin typing to search...';
+  @Output() search = new EventEmitter<{ search: string; page?: 1 }>();
+  input = new FormControl('', { nonNullable: true });
+
+  constructor(private unsubscribeService: UnsubscribeService) {}
+
+  ngOnInit(): void {
+    const inputSub = this.input.valueChanges
+      .pipe(
+        debounceTime(500),
+        map((search) => search.trim()),
+        distinctUntilChanged()
+      )
+      .subscribe((search) => this.search.emit({ search }));
+    this.unsubscribeService.addSubscription(inputSub);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeService.unsubscribeAll();
+  }
+}
