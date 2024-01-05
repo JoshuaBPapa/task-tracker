e4dfe4df79fce3f2ef88f47b26d63299e3e2da9e
JoshuaBPapa <joshua.b.papa@gmail.com>
2024-01-05 09:58:03 +0000

    created a toolbar component to apply flex styling to its content

---
+++ client/src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {}
