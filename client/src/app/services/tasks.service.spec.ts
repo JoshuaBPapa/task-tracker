930f5fd92f58e8e9b44a2b4b4ff145e86f5fa1d5
JoshuaBPapa <joshua.b.papa@gmail.com>
2024-02-02 09:22:18 +0000

    added a GET request for task and a method to create filters for task tables

--- client/src/app/services/tasks.service.spec.ts
+++ client/src/app/services/tasks.service.spec.ts
@@ -40,4 +40,59 @@ describe('TasksService', () => {
     expect(req.request.method).toBe('POST');
     expect(req.request.body).toEqual(mockTaskValues);
   });
+
+  it('getTasks should place a GET request with the correct url', () => {
+    service.getTasks({}).subscribe();
+    const req = httpMock.expectOne(`${environment.api}/tasks`);
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('createTaskFilters should return the correct filterConfig', () => {
+    expect(service.createTaskFilters()).toEqual([
+      {
+        filterName: 'Status',
+        filterKey: 'status',
+        options: [
+          {
+            key: 1,
+            label: 'Not Started',
+          },
+          {
+            key: 2,
+            label: 'In Progress',
+          },
+          {
+            key: 3,
+            label: 'In Review',
+          },
+          {
+            key: 4,
+            label: 'Complete',
+          },
+        ],
+      },
+      {
+        filterName: 'Priority',
+        filterKey: 'priority',
+        options: [
+          {
+            key: 1,
+            label: 'Low',
+          },
+          {
+            key: 2,
+            label: 'Medium',
+          },
+          {
+            key: 3,
+            label: 'High',
+          },
+          {
+            key: 4,
+            label: 'Severe',
+          },
+        ],
+      },
+    ]);
+  });
 });
--- client/src/app/services/tasks.service.ts
+++ client/src/app/services/tasks.service.ts
@@ -1,8 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { FilterDropdownConfig } from 'src/types/filter-dropdown-config/filter-dropdown-config';
+import { Page } from 'src/types/page';
+import { Params } from 'src/types/params/params';
 import { CreatedResponse } from 'src/types/responses/created-response';
+import { Task } from 'src/types/responses/task';
+import { TaskStatusPipe } from '../shared/pipes/task-status.pipe';
+import { TaskPriorityPipe } from '../shared/pipes/task-priority.pipe';
 
 interface PostTaskData {
   title: string;
@@ -15,9 +21,43 @@ interface PostTaskData {
 
 @Injectable()
 export class TasksService {
+  private tasksData = new Subject<Page<Task>>();
+  tasksData$ = this.tasksData.asObservable();
+
   constructor(private http: HttpClient) {}
+
+  getTasks(params: Params): Observable<Page<Task>> {
+    return this.http
+      .get<Page<Task>>(`${environment.api}/tasks`, {
+        params: { ...params },
+      })
+      .pipe(tap((res) => this.tasksData.next(res)));
+  }
 
   postTask(task: PostTaskData): Observable<CreatedResponse> {
     return this.http.post<CreatedResponse>(`${environment.api}/tasks`, task);
+  }
+
+  createTaskFilters(): FilterDropdownConfig[] {
+    const taskStatusPipe = new TaskStatusPipe();
+    const taskPriorityPipe = new TaskPriorityPipe();
+
+    const statusFilters: FilterDropdownConfig = {
+      filterName: 'Status',
+      filterKey: 'status',
+      options: [],
+    };
+    const priorityFilters: FilterDropdownConfig = {
+      filterName: 'Priority',
+      filterKey: 'priority',
+      options: [],
+    };
+
+    for (let i = 1; i <= 4; i++) {
+      statusFilters.options.push({ key: i, label: taskStatusPipe.transform(i, 'text') });
+      priorityFilters.options.push({ key: i, label: taskPriorityPipe.transform(i, 'text') });
+    }
+
+    return [statusFilters, priorityFilters];
   }
 }
