68154b6d46f9902c09f222c075e9b0e2aebefaf9
JoshuaBPapa <joshua.b.papa@gmail.com>
2024-02-15 17:38:59 +0000

    added edit and delete modals to task detail page

--- client/src/app/pages/tasks/task-details-container/task-details-container.component.spec.ts
+++ client/src/app/pages/tasks/task-details-container/task-details-container.component.spec.ts
@@ -1,15 +1,103 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskDetailsContainerComponent } from './task-details-container.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { CommentsService } from 'src/app/services/comments.service';
+import { Page } from 'src/types/page';
+import { Comment } from 'src/types/responses/comment';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { TaskDetailed } from 'src/types/responses/task';
+import { UnsubscribeService } from 'src/app/services/unsubscribe.service';
 
-xdescribe('TaskDetailsContainerComponent', () => {
+const mockTaskData: TaskDetailed = {
+  id: 1,
+  title: 'test',
+  status: 1,
+  priority: 4,
+  dateTimeCreated: '2023-08-02T15:01:00.000Z',
+  dateTimeUpdated: '2023-11-30T10:07:37.000Z',
+  assignedUser: {
+    id: 1,
+    jobTitle: 'Developer',
+    lastName: 'Doe',
+    firstName: 'John',
+    pictureColour: 'red',
+  },
+  description: '',
+  project: {
+    id: 1,
+    name: 'Mock project',
+  },
+};
+const mockCommentsData: Page<Comment> = {
+  results: [
+    {
+      id: 1,
+      comment: 'mock comment',
+      dateTimeCreated: '',
+      postedBy: {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        jobTitle: 'Developer',
+        pictureColour: 'red',
+      },
+    },
+  ],
+  page: 1,
+  total: 1,
+};
+const mockLoggedInUser = {
+  userId: 1,
+  firstName: 'mock value',
+  lastName: 'mock value',
+  username: 'mock value',
+  jobTitle: 'mock value',
+  teamId: 1,
+  authLevel: 1,
+  pictureColour: 'mock value',
+};
+
+describe('TaskDetailsContainerComponent', () => {
   let component: TaskDetailsContainerComponent;
   let fixture: ComponentFixture<TaskDetailsContainerComponent>;
+  const commentsServiceSpy = jasmine.createSpyObj(
+    'CommentsService',
+    ['getComments', 'postComment'],
+    {
+      commentsData$: of(mockCommentsData),
+    }
+  );
+  const authServiceSpy = jasmine.createSpyObj('AuthService', [], {
+    loggedInUser: mockLoggedInUser,
+  });
+  const unsubscribeServiceSpy = jasmine.createSpyObj('UnsubscribeService', [
+    'addSubscription',
+    'unsubscribeAll',
+  ]);
+  const activatedRouteStub = {
+    data: of({ task: mockTaskData }),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [TaskDetailsContainerComponent],
+      imports: [TaskDetailsContainerComponent, RouterTestingModule],
+      providers: [
+        provideHttpClient(),
+        MessageService,
+        { provide: ActivatedRoute, useValue: activatedRouteStub },
+        { provide: AuthService, useValue: authServiceSpy },
+      ],
     });
+    TestBed.overrideProvider(CommentsService, { useValue: commentsServiceSpy });
+    TestBed.overrideProvider(UnsubscribeService, { useValue: unsubscribeServiceSpy });
+
+    commentsServiceSpy.getComments.and.returnValue(of(mockCommentsData));
+
     fixture = TestBed.createComponent(TaskDetailsContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +105,49 @@ xdescribe('TaskDetailsContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit should set the value of task and commentsData$ and call subscribeToCommentsPageNumber', () => {
+    spyOn(component, 'subscribeToCommentsPageNumber');
+    component.ngOnInit();
+    expect(component.task).toEqual(mockTaskData);
+    component.commentsData$.subscribe((res) => expect(res).toEqual(mockCommentsData));
+    expect(component.subscribeToCommentsPageNumber).toHaveBeenCalled();
+  });
+
+  it('subscribeToCommentsPageNumber should call unsubscribeService.addSubscription', () => {
+    component.subscribeToCommentsPageNumber();
+    expect(unsubscribeServiceSpy.addSubscription).toHaveBeenCalled();
+  });
+
+  it('ngOnDestroy should call unsubscribeService.unsubscribeAll', () => {
+    component.ngOnDestroy();
+    expect(unsubscribeServiceSpy.unsubscribeAll).toHaveBeenCalled();
+  });
+
+  it('onOpenEditModal should set isEditModalVisible to true', () => {
+    component.onOpenEditModal();
+    expect(component.isEditModalVisible).toBeTrue();
+  });
+
+  it('handleEditModalClose should set isEditModalVisible to false', () => {
+    component.handleEditModalClose();
+    expect(component.isEditModalVisible).toBeFalse();
+  });
+
+  it('onOpenDeleteModal should set isDeleteModalVisible to true', () => {
+    component.onOpenDeleteModal();
+    expect(component.isDeleteModalVisible).toBeTrue();
+  });
+
+  it('handleDeleteModalClose should set isDeleteModalVisible to false', () => {
+    component.handleDeleteModalClose();
+    expect(component.isDeleteModalVisible).toBeFalse();
+  });
+
+  it('onCommentsPageChange should call commentsPageNumber.next with the correct value', () => {
+    spyOn(component.commentsPageNumber, 'next');
+    component.onCommentsPageChange({ page: 2 });
+    expect(component.commentsPageNumber.next).toHaveBeenCalledWith({ page: 2 });
   });
 });
--- client/src/app/pages/tasks/task-details-container/task-details-container.component.ts
+++ client/src/app/pages/tasks/task-details-container/task-details-container.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject, Observable, catchError, switchMap, tap } from 'rxjs';
 import { CommentListComponent } from 'src/app/components/comments/comment-list/comment-list.component';
 import { PostCommentComponent } from 'src/app/components/comments/post-comment/post-comment.component';
 import { LoadingSpinnerComponent } from 'src/app/components/loading-spinner/loading-spinner.component';
+import { DeleteModalComponent } from 'src/app/components/modals/delete-modal/delete-modal.component';
+import { TaskFormModalComponent } from 'src/app/components/modals/task-form-modal/task-form-modal.component';
 import { PaginatorComponent } from 'src/app/components/paginator/paginator.component';
 import { TaskPriorityTagComponent } from 'src/app/components/tags/task-priority-tag/task-priority-tag.component';
 import { TaskStatusTagComponent } from 'src/app/components/tags/task-status-tag/task-status-tag.component';
@@ -12,10 +14,13 @@ import { AssignedUserComponent } from 'src/app/components/user/assigned-user/ass
 import { CommentsService } from 'src/app/services/comments.service';
 import { ErrorHandlingService } from 'src/app/services/error-handling.service';
 import { FormValidationService } from 'src/app/services/form-validation.service';
+import { ModalDataService } from 'src/app/services/modal-data.service';
+import { TasksService } from 'src/app/services/tasks.service';
 import { UnsubscribeService } from 'src/app/services/unsubscribe.service';
 import { TextTruncateDirective } from 'src/app/shared/directives/text-truncate.directive';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CommentForm } from 'src/types/forms/comment-form';
+import { TaskForm } from 'src/types/forms/task-form';
 import { Page } from 'src/types/page';
 import { Comment } from 'src/types/responses/comment';
 import { TaskDetailed } from 'src/types/responses/task';
@@ -33,8 +38,10 @@ import { TaskDetailed } from 'src/types/responses/task';
     PaginatorComponent,
     LoadingSpinnerComponent,
     TextTruncateDirective,
+    DeleteModalComponent,
+    TaskFormModalComponent,
   ],
-  providers: [CommentsService, UnsubscribeService],
+  providers: [CommentsService, UnsubscribeService, TasksService],
   templateUrl: './task-details-container.component.html',
   styleUrls: ['./task-details-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -44,13 +51,18 @@ export class TaskDetailsContainerComponent implements OnInit, OnDestroy {
   isCommentsLoading = new BehaviorSubject(false);
   commentsPageNumber = new BehaviorSubject<{ page: number }>({ page: 1 });
   commentsData$: Observable<Page<Comment>>;
+  isEditModalVisible = false;
+  isDeleteModalVisible = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private commentsService: CommentsService,
     private unsubscribeService: UnsubscribeService,
     private errorHandlingService: ErrorHandlingService,
-    private formValidationService: FormValidationService
+    private formValidationService: FormValidationService,
+    private modalDataService: ModalDataService,
+    private tasksService: TasksService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -62,7 +74,7 @@ export class TaskDetailsContainerComponent implements OnInit, OnDestroy {
   }
 
   subscribeToCommentsPageNumber(): void {
-    this.commentsPageNumber
+    const sub = this.commentsPageNumber
       .pipe(
         tap(() => this.isCommentsLoading.next(true)),
         switchMap((pageNumberParam) => {
@@ -76,6 +88,7 @@ export class TaskDetailsContainerComponent implements OnInit, OnDestroy {
         })
       )
       .subscribe(() => this.isCommentsLoading.next(false));
+    this.unsubscribeService.addSubscription(sub);
   }
 
   onCommentPost(commentForm: FormGroup<CommentForm>): void {
@@ -98,6 +111,45 @@ export class TaskDetailsContainerComponent implements OnInit, OnDestroy {
 
   onCommentsPageChange(page: { page: number }): void {
     this.commentsPageNumber.next(page);
+  }
+
+  onOpenEditModal(): void {
+    this.isEditModalVisible = true;
+  }
+
+  handleEditModalClose(): void {
+    this.isEditModalVisible = false;
+  }
+
+  handleEditModalSubmit(form: FormGroup<TaskForm>): void {
+    if (!this.formValidationService.checkIsFormValid(form)) return;
+    const formValue = form.getRawValue();
+
+    this.modalDataService
+      .sendRequest(this.tasksService.putTask(formValue, this.task.id), 'Task Updated', form)
+      .subscribe(() => {
+        this.handleEditModalClose();
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigateByUrl(`/tasks/${this.task.id}`);
+        });
+      });
+  }
+
+  onOpenDeleteModal(): void {
+    this.isDeleteModalVisible = true;
+  }
+
+  handleDeleteModalClose(): void {
+    this.isDeleteModalVisible = false;
+  }
+
+  handleDeleteModalConfirm(): void {
+    this.modalDataService
+      .sendRequest(this.tasksService.deleteTask(this.task.id), 'Task Deleted')
+      .subscribe(() => {
+        this.handleDeleteModalClose();
+        this.router.navigateByUrl('/tasks');
+      });
   }
 
   ngOnDestroy(): void {
