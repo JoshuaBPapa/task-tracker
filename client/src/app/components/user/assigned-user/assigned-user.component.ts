4f01db6802cddc91fb89dbff22ab1e1be0add15d
JoshuaBPapa <joshua.b.papa@gmail.com>
2024-02-21 14:57:24 +0000

    wrapped the assigned user component in an anchor tag to link to the user's page

--- client/src/app/components/user/assigned-user/assigned-user.component.ts
+++ client/src/app/components/user/assigned-user/assigned-user.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { UserIconComponent } from '../user-icon/user-icon.component';
 import { AssignedUser } from 'src/types/assigned-user';
 import { NamePipe } from 'src/app/shared/pipes/name.pipe';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-assigned-user',
   standalone: true,
-  imports: [CommonModule, UserIconComponent, NamePipe],
+  imports: [SharedModule, UserIconComponent, NamePipe],
   templateUrl: './assigned-user.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
